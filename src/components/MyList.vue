7ffe2eb9b57443b59215bf0c3f8263e163a702de
Tohsaka <1679124358@qq.com>
2022-09-18 14:41:46 +0800

    1.0

---
+++ src/components/MyList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="box">
+    <MyItem v-for="(todoObj) in todos" :key="todoObj.id" :todo="todoObj"></MyItem>
+  </div>
+</template>
+
+<script>
+import MyItem from './MyItem.vue'
+export default {
+name:'MyList',
+components:{
+    MyItem
+},
+props:['todos'],
+methods:{
+
+}
+}
+</script>
+
+<style scoped>
+.box{
+  padding: 5px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 20px;
+  width: 90%;
+  min-height: 300px;
+  box-shadow: 0 0 8px black;
+  border-radius: 10px
+}
+</style>
